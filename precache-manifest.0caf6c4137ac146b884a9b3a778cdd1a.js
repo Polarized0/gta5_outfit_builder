627b97733db656df5a13da691a97729a8e592981
Polarized0 <none>
2021-04-15 09:23:20 +0200

    Updates

---
+++ precache-manifest.0caf6c4137ac146b884a9b3a778cdd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45723792d690200b90132445a39a81b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57ee3bf7e32682787dfc",
+    "url": "/static/css/main.3519037b.chunk.css"
+  },
+  {
+    "revision": "5074173cfd5c9cfecac5",
+    "url": "/static/js/2.dd030f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd030f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ee3bf7e32682787dfc",
+    "url": "/static/js/main.af04f8f7.chunk.js"
+  },
+  {
+    "revision": "a1e4772f3ec1c1af4e65",
+    "url": "/static/js/runtime-main.a20755e0.js"
+  }
+]);
