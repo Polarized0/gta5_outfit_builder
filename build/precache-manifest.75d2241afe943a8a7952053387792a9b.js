20475358a649498c13ce657fe11f501e5eee6e13
Polarized0 <none>
2021-04-15 08:26:49 +0200

    Add build

---
+++ build/precache-manifest.75d2241afe943a8a7952053387792a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71190be0c80f8f996e0ad14f40e3fc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46792f318b5af4f18815",
+    "url": "/static/css/main.7f84f8bc.chunk.css"
+  },
+  {
+    "revision": "5074173cfd5c9cfecac5",
+    "url": "/static/js/2.dd030f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd030f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46792f318b5af4f18815",
+    "url": "/static/js/main.ff66e598.chunk.js"
+  },
+  {
+    "revision": "a1e4772f3ec1c1af4e65",
+    "url": "/static/js/runtime-main.a20755e0.js"
+  }
+]);
